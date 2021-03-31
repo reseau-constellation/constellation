d377b0686fded3d3ff61446e223ec396622f7261
julienmalard <julien.malard@mail.mcgill.ca>
2021-03-31 09:45:03 -0400

    Avant de déployer

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -8,18 +8,18 @@ const fs = require("fs");
     await execa("git", ["commit", "-a", "-m", "Avant de déployer"]);
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
-    console.log("Building started...");
+    console.log("On construit le projet...");
     await execa("yarn", ["build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-    console.log("Pushing to gh-pages...");
+    console.log("On l'envoie à gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
-    console.log("Successfully deployed, check your settings");
+    console.log("Déployé avec succès ; allez voir sur GitHub maintenant !");
   } catch (e) {
     // eslint-disable-next-line no-console
     console.log(e.message);
