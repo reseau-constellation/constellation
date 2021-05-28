4160e5b1b0c8bc1e418d580fce2dea53ace01a02
julienmalard <julien.malard@mail.mcgill.ca>
2021-05-28 15:14:50 -0400

    Avant de déployer

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const webpack = require("webpack");
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/constellation/" : "/",
   transpileDependencies: ["vuetify"],
+  productionSourceMap: false,
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
