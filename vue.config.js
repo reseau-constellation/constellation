31bff0f8f9656a1259437ed0342b776ea44af050
julienmalard <julien.malard@mail.mcgill.ca>
2022-01-25 12:30:32 +0100

    Changer configurationn de `publicPath` pour le nouveau domaine

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/constellation/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   transpileDependencies: ["vuetify"],
   productionSourceMap: false,
   configureWebpack: {
