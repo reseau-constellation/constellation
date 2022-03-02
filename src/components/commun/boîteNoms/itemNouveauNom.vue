f2fe8029616bd5afa0cbc5c05b6d38a95e3c19a6
sheeja <sheejakumar.d@gmail.com>
2022-03-02 06:26:45 +0530

    திருத்தம்

--- src/components/commun/boîteNoms/itemNouveauNom.vue
+++ src/components/commun/boîteNoms/itemNouveauNom.vue
@@ -4,7 +4,7 @@
       <v-row>
         <v-col cols="4">
           <v-select
-            :label="$t('itemNouveauNom.etiquetteLangue')"
+            :label="$t('itemNouveauNom.étiquetteLangue')"
             v-model="langueNouveauNom"
             outlined
             dense
@@ -16,7 +16,7 @@
         <v-col cols="8">
           <v-text-field
             v-model="nouveauNom"
-            :label="$t('itemNouveauNom.etiquetteNom')"
+            :label="$t('itemNouveauNom.étiquetteNom')"
             :dir="droiteÀGauche(langueNouveauNom) ? 'rtl' : 'ltr'"
             outlined
             dense
