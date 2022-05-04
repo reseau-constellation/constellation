3223f1c0bb2361264f09c5aa396e2bf8563204d5
sheejakumar <sheejakumar.d@gmail.com>
2022-05-04 11:24:44 +0530

    கோப்பு தேர்வு

--- src/components/commun/dialogueImporter.vue
+++ src/components/commun/dialogueImporter.vue
@@ -31,7 +31,7 @@
      <v-stepper-content step="1">
        <v-card
        class="mb-2"
-       height="200px"
+       height="300px"
        >
        <v-btn
           color="primary"
@@ -49,31 +49,30 @@
           @change="onFilePicked"/>
        </v-btn>
        <div class="text-center">
-
         <v-chip
         v-if="nomFichier"
         class="ma-2"
         close
         color="green"
         outlined
+        :disabled="!Sélectionner"
         @click:close="nomFichier= false"
-         >
-        nomFichier
-        </v-chip>
-      </div>
-
+       >
+      {{nomFichier}}
+      </v-chip>
+    </div>
        <v-container fluid>
          <v-row align="center">
            <v-col
            cols="12"
            sm="12">
            <v-select
-           v-model="e7"
-           :items="அட்டவணை"
+           v-model="Sélectionner"
+           :items="itemsSélectionner"
            :menu-props="{ maxHeight: '75' }"
            :label="$t('communs.தேர்ந்தெடுக்கவும்')"
            multiple
-           hint="பயனாளர் ஒரு அட்டவணையை தேர்ந்தெடுக்கவும்"
+           :disabled="!suivante"
            ></v-select>
          </v-col>
        </v-row>
@@ -82,37 +81,35 @@
      <v-btn
        color="primary"
        @click="e6 = 2"
-       :disabled="!onPickFile"
        >
        {{ $t("communs.suivante") }}
     </v-btn>
   </v-card>
  </v-stepper-content>
-   <v-stepper-content step="2">
-     <v-card
-       class="mb-2"
-       height="70px"
+ <v-stepper-content step="2">
+<v-card
+  class="mb-2"
+  height="70px"
+  >
+  <v-btn
+     color="primary"
+       text
+       outlined
+       :loading="enProgrès"
+       @click="() => obtColsTableau()"
+       :disabled="!suivante"
        >
-       <v-btn
-          color="primary"
-            text
-            outlined
-            :loading="enProgrès"
-            @click="() => obtColsTableau()"
-            >
-            {{ $t("communs.colonne") }}
-
-         </v-btn>
-
-         <v-btn
-         color="primary"
-         @click="e6 = 3"
-         :disabled="!colonne"
-         >
-        {{ $t("communs.suivante") }}
-         </v-btn>
-       </v-card>
-     </v-stepper-content>
+       {{ $t("communs.colonne") }}
+    </v-btn>
+    <v-btn
+    color="primary"
+    @click="e6 = 3"
+    :disabled="!colonne"
+    >
+   {{ $t("communs.suivante") }}
+    </v-btn>
+  </v-card>
+</v-stepper-content>
      <v-stepper-content step="3">
        <v-card
          class="mb-2"
@@ -158,7 +155,7 @@
 <script lang="ts">
 import ImportateurFeuilleCalcul from "@/components/commun/xlsx";
 import mixins from "vue-typed-mixins";
-import XLSX from "xlsx";
+import XLSX, { read as readXLSX } from "xlsx";
 import mixinLangues from "@/mixins/langues";
 export default mixins(mixinLangues).extend({
   name: "dialogueTéléverser",
@@ -174,92 +171,48 @@ export default mixins(mixinLangues).extend({
       e6: 1,
       importateur: undefined as ImportateurFeuilleCalcul | undefined,
       tableauxFichier: undefined as string[] | undefined,
-      e7: [],
-      அட்டவணை:['sheet1','sheet2','sheet3','sheet4','sheet5',],
+      Sélectionner: [],
+      itemsSélectionner:['sheet1','sheet2','sheet3','sheet4','sheet5',],
       nomFichier: undefined as string | undefined,
     };
   },
 
   methods: {
     onFilePicked: async function(): Promise<void> {
-      const fichiers = this.$refs.fileInput as HTMLInputElement // தேர்ந்தெடுத்தப்பட்டு கோப்பு
+      console.log("onFilePicked")
+      const fichiers = this.$refs.fileInput as HTMLInputElement; // தேர்ந்தெடுத்தப்பட்டு கோப்பு
+      console.log("fichiers",fichiers )
       if (!fichiers.files) return
-      const données = await fichiers!.files[0].arrayBuffer() // கோப்பில் உள்ள தகவல்கள்
-      const doc = XLSX.read(données) // கோப்பை யாவாக்கிறீட்டில் திறக்கவும்
-
-      this.importateur = new ImportateurFeuilleCalcul(doc) // விண்மீன் மூலம் பகுப்பாய்வு செய்யலாம்
-      this.nomFichier = fichiers!.files[0].name
-      console.log("வணக்கம்", this.nomFichier)
+      const données = await fichiers!.files[0].arrayBuffer(); // கோப்பில் உள்ள தகவல்கள்
+      console.log("données",données )
+      const doc = readXLSX(données, {
+       type: "buffer",
+       cellDates: true,
+       }) ;// கோப்பை யாவாக்கிறீட்டில் திறக்கவும்
+      console.log("doc",doc)
+      this.importateur = new ImportateurFeuilleCalcul(doc); // விண்மீன் மூலம் பகுப்பாய்வு செய்யலாம்
+      console.log("this.importateur",this.importateur)
+      this.nomFichier = fichiers!.files[0].name;
+      console.log("this.nomFichier",this.nomFichier)
       },
      onPickFile: function ():void {
+       console.log("onPickFile" )
         const fileInput = this.$refs.fileInput as HTMLInputElement
         fileInput.click()
       },
-      watch: {
-        importateur: async function (val: ImportateurFeuilleCalcul | undefined )
+    },
+
+  watch: {
+    importateur: async function (val: ImportateurFeuilleCalcul | undefined )
         {
          if (val) {
            this.tableauxFichier = val.obtNomsTableaux()
             }
             else {
               this.tableauxFichier = undefined
-            }
-          },
-       },
-
-    téléverser: async function () {
-      this.enProgrès = true;
-      try {
-        switch (this.type) {
-          case "bd": {
-            const données = await this.$ipa.bds!.exporterDonnées(
-              this.id,
-              this.languesPréférées
-            );
-            this.$ipa.bds!.exporterDocumentDonnées(
-              données,
-              this.formatDoc,
-              undefined,
-              this.inclureMédias
-            );
-            break;
-          }
-          case "tableau": {
-            const données = await this.$ipa.tableaux!.exporterDonnées(
-              this.id,
-              this.languesPréférées
-            );
-            this.$ipa.bds!.exporterDocumentDonnées(
-              données,
-              this.formatDoc,
-              undefined,
-              this.inclureMédias
-            );
-            break;
-          }
-          case "projet": {
-            const données = await this.$ipa.projets!.exporterDonnées(
-              this.id,
-              this.languesPréférées
-            );
-            this.$ipa.projets!.exporterDocumentDonnées(
-              données,
-              this.formatDoc,
-              undefined,
-              this.inclureMédias
-            );
-            break;
-          }
-          default:
-            throw new Error(`"${this.type}" inconnu`);
-        }
-      } catch {
-        // Rien à faire
-      }
-      this.enProgrès = false;
-    },
-  },
-});
+           }
+        },
+     },
+  });
 </script>
-
 <style></style>
