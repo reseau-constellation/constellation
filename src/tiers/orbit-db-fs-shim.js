b06a3b3b0a5a6b4dce27d486dde890ddada98198
julienmalard <julien.malard@mail.mcgill.ca>
2022-05-05 09:42:03 +0200

    Plus besoin de ça

--- src/tiers/orbit-db-fs-shim.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-const oùSommesNous = require('wherearewe')
-
-const fs = (!oùSommesNous.isElectronMain && (typeof window === 'object' || typeof self === 'object')) ? null : eval('require("fs")')
-
-module.exports = fs
