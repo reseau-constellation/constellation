e1d4a5ba2b5fea569903af0b75186d823086884d
julienmalard <julien.malard@mail.mcgill.ca>
2022-06-09 16:19:24 +0530

    Progrès carte utilisatrices

---
+++ src/utils/événements.ts
@@ -0,0 +1,22 @@
+import mitt from 'mitt';
+
+const emitter = mitt();
+
+/*
+// listen to an event
+emitter.on('foo', e => console.log('foo', e) )
+
+// listen to all events
+emitter.on('*', (type, e) => console.log(type, e) )
+
+// fire an event
+emitter.emit('foo', { a: 'b' })
+
+// clearing all events
+emitter.all.clear()
+
+// working with handler references:
+function onFoo() {}
+emitter.on('foo', onFoo)   // listen
+emitter.off('foo', onFoo)  // unlisten
+*/
