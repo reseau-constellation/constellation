0bd15b91a5907f002e4ef2b50447e9cddd95bae8
julienmalard <julien.malard@mail.mcgill.ca>
2022-10-03 11:46:38 +0200

    Mise à jour execa

--- scripts/gh-pages-deploy.mjs
+++ scripts/gh-pages-deploy.mjs
@@ -8,29 +8,29 @@ const thirdLog = `${emoji.get('rocket')} ${chalk.green('Your app successfully de
 
 ;(async () => {
   try {
-    const { stdout: currentBranch } = await execa('git branch --show-current')
-    await execa('git checkout --orphan gh-pages')
+    const { stdout: currentBranch } = await execa('git', ['branch', '--show-current'])
+    await execa('git', ['checkout', '--orphan gh-pages'])
     console.log(firstLog)
-    await execa('yarn build', { stdio: 'inherit' })
+    await execa('yarn', ['build'], { stdio: 'inherit' })
 
     // Idée de https://stackoverflow.com/questions/48521177/404-when-reloading-a-vue-website-published-to-github-pages
-    await execa('cp dist/index.html dist/404.html')
+    await execa('cp', ['dist/index.html', 'dist/404.html'])
 
     // Idée de https://stackoverflow.com/questions/66311145/github-pages-custom-domain-settings-gets-reset-during-new-commit
     // Afin de garder le domaine réseau-constellation.ca dans le déploiement Github
-    await execa('touch dist/CNAME')
-    await execa('echo "xn--rseau-constellation-bzb.ca" > dist/CNAME', { stdio: 'inherit', shell: true })
-    await execa('ls dist', { stdio: 'inherit' })
-    await execa('cat dist/CNAME', { stdio: 'inherit' })
+    await execa('touch', ['dist/CNAME'])
+    await execa('echo', ['"xn--rseau-constellation-bzb.ca" > dist/CNAME'], { stdio: 'inherit', shell: true })
+    await execa('ls', ['dist'], { stdio: 'inherit' })
+    await execa('cat', ['dist/CNAME'], { stdio: 'inherit' })
 
-    await execa('git --work-tree dist add --all')
-    await execa('git --work-tree dist commit -m "gh-pages"')
+    await execa('git', ['--work-tree', 'dist', 'add', '--all'])
+    await execa('git', ['--work-tree', 'dist', 'commit', '-m', '"gh-pages"'])
     console.log(secondLog)
 
-    await execa('git push origin HEAD:gh-pages --force', { stdio: 'inherit' })
-    await execa('rm -r dist')
-    await execa(`git checkout -f ${currentBranch}`)
-    await execa('git branch -D gh-pages')
+    await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force'], { stdio: 'inherit' })
+    await execa('rm', ['-r', 'dist'])
+    await execa('git', ['checkout', '-f', currentBranch])
+    await execa('git', ['branch', '-D', 'gh-pages'])
     console.log(thirdLog)
 
   } catch (e) {
